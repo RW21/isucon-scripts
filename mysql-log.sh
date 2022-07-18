6e88e3fc712f4a51965fedbf34bee886bf62f28c
RW21 <rinze.watanabe.translation@gmail.com>
2022-07-19 08:13:25 +0900

    Update mysql-log.sh

--- mysql-log.sh
+++ mysql-log.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-mysqldumpslow /var/log/mysql/mysql-slow.log -s t | less
-
+mkdir -p /var/log/previous-logs
+filename=/var/log/previous-logs/$(date +"%H-%M-%S").mysql-log
+echo $filename
+mysqldumpslow /var/log/mysql/mysql-slow.log -s t > $filename
+less $filename
